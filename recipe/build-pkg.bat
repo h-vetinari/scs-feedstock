b5e9874cc8e5e2b950922da5f2a6b281ed21e61a
H. Vetinari <h.vetinari@gmx.com>
2021-06-06 20:18:00 +0200

    set BLAS env vars also for windows

--- recipe/build-pkg.bat
+++ recipe/build-pkg.bat
@@ -1,3 +1,7 @@
+
+set "BLAS_LAPACK_LIB_PATHS=%LIBRARY_LIB%"
+set "BLAS_LAPACK_LIBS=lapack:cblas:blas"
+
 if "%cuda_compiler_version%"=="None" (
     python -m pip install . -vv
     if %ERRORLEVEL% neq 0 exit 1
